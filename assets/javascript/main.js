e6c6df1e935197859d144c57f5e3aa2665cf99e7
Sonny <richardhughesjr@gmail.com>
2018-01-20 03:03:35 -0600

    Merged input fields and added geo dropdown to submission form

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,41 +1,37 @@
 $(document).ready(function(){
     
     // Firebase
-    var trainData = new Firebase("https://nwuproject1.firebaseio.com/");
+    var dogData = new Firebase("https://nwuproject1.firebaseio.com/");
 
-    // Button to add Trains
-    $("#addTrainButton").on("click", function(){
+    // Button to add Dogs
+    $("#addDog").on("click", function(){
 
         // Parses input values and attaches them to a variable
-        var trainName = $("#trainNameInput").val().trim();
+        var dogName = $("#dogNameInput").val().trim();
         var lineName = $("#lineNumberInput").val().trim();
-        var destination = $("#routeInput").val().trim();
-        var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");;
-        var frequencyInput = $("#frequencyInput").val().trim();
+        var destination = $("#addr").val().trim();
+        var dogTimeInput = moment($("#dogTimeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");;
 
         // Check variable inputs
-        console.log(trainName);
+        console.log(dogName);
         console.log(lineName);
         console.log(destination);
-        console.log(trainTimeInput);
-        console.log(frequencyInput);
+        console.log(dogTimeInput);
 
         // Creates object from input fields and pushes to firebase
-        var newTrain = {
-            name:  trainName,
+        var newDog = {
+            name:  dogName,
             line: lineName,
             destination: destination,
-            trainTime: trainTimeInput,
-            frequency: frequencyInput,
+            dogTime: dogTimeInput,
         }
-        trainData.push(newTrain);
+        dogData.push(newDog);
 
         // Resets the input values after data is pushed
-        $("#trainNameInput").val("");
+        $("#dogNameInput").val("");
         $("#lineNumberInput").val("");
-        $("#routeInput").val("");
-        $("#trainTimeInput").val("");
-        $("#frequencyInput").val("");
+        $("#addr").val("");
+        $("#dogTimeInput").val("");
 
         // Prevents page refresh
         return false;
@@ -43,7 +39,7 @@ $(document).ready(function(){
 
 
     // Event function 
-    trainData.on("child_added", function(childSnapshot, prevChildKey){
+    dogData.on("child_added", function(childSnapshot, prevChildKey){
 
         console.log(childSnapshot.val());
 
@@ -51,24 +47,24 @@ $(document).ready(function(){
         var firebaseName = childSnapshot.val().name;
         var firebaseLine = childSnapshot.val().line;
         var firebaseDestination = childSnapshot.val().destination;
-        var firebaseTrainTimeInput = childSnapshot.val().trainTime;
+        var firebaseDogTimeInput = childSnapshot.val().dogTime;
         var firebaseFrequency = childSnapshot.val().frequency;
         
-        var diffTime = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes");
-        var timeRemainder = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes") % firebaseFrequency ;
+        var diffTime = moment().diff(moment.unix(firebaseDogTimeInput), "minutes");
+        var timeRemainder = moment().diff(moment.unix(firebaseDogTimeInput), "minutes") % firebaseFrequency ;
         var minutes = firebaseFrequency - timeRemainder;
 
-        var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A"); 
+        var nextDogArrival = moment().add(minutes, "m").format("hh:mm A"); 
         
         // Test for correct times and info
         console.log(minutes);
-        console.log(nextTrainArrival);
+        console.log(nextDogArrival);
         console.log(moment().format("hh:mm A"));
-        console.log(nextTrainArrival);
+        console.log(nextDogArrival);
         console.log(moment().format("X"));
 
-        // Append train info to table on page
-        $("#trainTable > tbody").append("<tr><td>" + firebaseName + "</td><td>" + firebaseLine + "</td><td>"+ firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
+        // Append dog info to table on page
+        $("#dogTable > tbody").append("<tr><td>" + firebaseName + "</td><td>" + firebaseLine + "</td><td>"+ firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + nextDogArrival + "</td><td>" + minutes + "</td></tr>");
 
     });
 });
